ef5680aab9302c27d3d1a03ebe796a1cfcfb7ae6
422106685 <422106685@pcpum.acatlan.unam.mx>
2023-03-31 12:53:49 -0600

    Initial Commit

---
+++ crear github/crear_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  crear_githubApp.swift
+//  crear github
+//
+//  Created by CEDAM07 on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct crear_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
